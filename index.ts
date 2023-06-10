3446bed12c8676b93670332890c6e5ec1dca0953
LyonSyonII <slimystudios@protonmail.com>
2023-06-10 20:31:21 +0200

    Converted to integration to copy files during build

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ export default function preload(): AstroIntegration {
         name: PKG_NAME,
         hooks: {
             "astro:build:start": async () => {
+                console.log(await fs.readdir("."));
                 const files = await fs.readdir("public/assets/preloaded");
                 await Promise.all(files.map(async file => fs.copyFile(`public/assets/preloaded/${file}`, `dist/assets/preloaded/${file}`, fs.constants.COPYFILE_EXCL)));
             }
